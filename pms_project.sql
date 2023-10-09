baaf220a2ae9f843402799ff25394e523ff2a880
Reetu495 <144803756+Reetu495@users.noreply.github.com>
2023-10-09 23:08:56 +0530

    Update pms_project.sql

--- pms_project.sql
+++ pms_project.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Aug 06, 2023 at 11:33 PM
--- Server version: 5.6.51-log
--- PHP Version: 7.4.26
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
